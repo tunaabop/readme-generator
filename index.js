4ec61f6943687ebc271cd1fa0933652f91956f89
tunaabop <tinawang158@gmail.com>
2023-02-23 15:29:40 -0800

    fixed readme template syntax/internal link syntax

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ const questions = [
 function generateREADME({title, description, installation, usage, contribute, testing, github, email}){
   return `# ${title}
 
-  ## Description
+  ## Description <a name="description"/>
   
-  ###Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+  ### Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
   
   - What was your motivation?
   - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
@@ -74,38 +74,38 @@ function generateREADME({title, description, installation, usage, contribute, te
   - [Usage](#usage)
   - [Contributing Guidelines](#contribute)
   - [Testing Instructions](#testing)
-  - [Questions/Contact] (#questions)
+  - [Questions/Contact](#questions)
   
-  <a name="installation"/>
-  ## Installation
+  
+  ## Installation <a name="installation"/>
   
   ### What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
   
   ${installation}
   
-  <a name="usage"/>
-  ## Usage
+ 
+  ## Usage  <a name="usage"/>
   
   ### Provide instructions and examples for use. Include screenshots as needed.
  
   ${usage}
 
-  <a name="contribute"/>
-  ## How to Contribute
+  
+  ## How to Contribute <a name="contribute"/>
   
   ### If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
   
   ${contribute}
   
-  <a name="testing"/>
-  ## Tests
+  
+  ## Tests <a name="testing"/>
   
   ### Go the extra mile and write tests for your application. Then provide examples on how to run them here.
   
   ${testing}
   
-  <a name="questions"/>
-  ## Questions? 
+  
+  ## Questions? <a name="questions"/>
     Reach me by email or github:
   ### Email: ${email}
   ### Github Profile: ${github}
